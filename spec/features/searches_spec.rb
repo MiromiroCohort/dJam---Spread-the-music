e84996f8dc807d63dc1c5407bfa26eb8fa1b4664
Denis Tyurkov <dtyurkov@gmail.com>
2015-08-25 11:28:48 +1200

    add search form existence, completion and submission tests

--- spec/features/searches_spec.rb
+++ spec/features/searches_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.feature "Searches", type: :feature do
+  scenario 'user can see the form on the page' do
+    visit new_search_path
 
+    expect(page).to have_selector('form')
+  end
+
+  scenario 'user can complete and submit a search' do
+    visit new_search_path
+
+    fill_in 'Search', with: "Nicolas Jaar el bandido"
+
+    click_button 'Search'
+
+    expect(page).to have_content('found')
+  end
 end
