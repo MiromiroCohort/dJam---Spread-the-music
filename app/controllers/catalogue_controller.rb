afc5b80b8c4b2f12ffb2948e1a356ea8e745eb57
Del Stevens <del.stevens@gmail.com>
2015-08-26 14:33:20 +1200

    data table and model Track created

--- app/controllers/catalogue_controller.rb
+++ app/controllers/catalogue_controller.rb
@@ -6,13 +6,12 @@ class CatalogueController
   html_out = "<table><tr><th>Artist</th><th>Title</th><th>Track length</th></tr>"
   music_dir = '/home/del/music_temp'
   Dir.foreach(music_dir) do |item|
-     next if item == '.' or item == '..'
-     this_track = Mp3Info.open("#{music_dir}/#{item}")
-     if this_track.tag.artist 
-      html_out << "<tr><td>" + this_track.tag.artist + "</td>"
-      html_out << "<td>" + this_track.tag.title + "</td>"
-      html_out << "<td>" + this_track.length.to_i.to_s + "</td>"
-      html_out << "</tr>"
+    next if item == '.' or item == '..'
+    this_track = Mp3Info.open("#{music_dir}/#{item}")
+    if this_track.tag.artist 
+      db_track = Track.new
+      db_track.artist = this_track.tag.artist
+      db_track.save
     end
   end
   html_out << "</table>"
