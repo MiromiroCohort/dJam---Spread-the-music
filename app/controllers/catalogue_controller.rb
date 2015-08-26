c35ea86f028a90ebb9e6e14b043d2bd625a1985a
Del Stevens <del.stevens@gmail.com>
2015-08-26 21:57:45 +1200

    timed delivery of top voted track

--- app/controllers/catalogue_controller.rb
+++ app/controllers/catalogue_controller.rb
@@ -1,18 +1,41 @@
 require 'mp3info'
 
+
+@party_over = false
+
 class CatalogueController
-  music_dir = '/home/del/music_temp'
-  Dir.foreach(music_dir) do |item|
-    next if item == '.' or item == '..'
-    this_track = Mp3Info.open("#{music_dir}/#{item}")
-    if this_track.tag.artist 
-             thisTrack = Track.create playlist_id:0, 
-             playlist_position: 0, 
-             artist: this_track.tag.artist,
-             title: this_track.tag.title, 
-             length:this_track.length, 
-             filename: item, 
-             vote_count: 0
+
+  def import_library
+    music_dir = '/home/del/music_temp'
+    Dir.foreach(music_dir) do |item|
+      next if item == '.' or item == '..'
+      this_track = Mp3Info.open("#{music_dir}/#{item}")
+      if this_track.tag.artist 
+               thisTrack = Track.create playlist_id:0, 
+               playlist_position: 0, 
+               artist: this_track.tag.artist,
+               title: this_track.tag.title, 
+               length:this_track.length, 
+               filename: item, 
+               vote_count: 0
+      end
     end
   end
+
+
+  def play_the_set
+    count = 1000
+    song_ctl = SongController.new
+    while count >0 do 
+      delay = song_ctl.play_top
+      p delay
+      delay = 20
+      x = Thread.new { sleep delay }  
+      x.join
+      count -=1
+    end
+  end
+
+
+
 end
--- app/controllers/song_controller.rb
+++ app/controllers/song_controller.rb
@@ -5,11 +5,6 @@ class SongController < ApplicationController
   @host_address = '127.0.0.1'
 
   def play(artist, song_string)
-    this_host = @host_address
-    session = Net::SSH.start( this_host, 'djam', :password => "C#ristmas25" )
-    # session = Net::SSH.start( '192.168.1.34', 'djam', :password => "C#ristmas25" )
-    session.exec "mpc clear ; mpc search artist \"#{artist}\" | grep \'#{song_string}\' | mpc add ; mpc play"
-    session.close
   end
 
   def stop
@@ -21,17 +16,25 @@ class SongController < ApplicationController
 
   
   def play_top
-    highest = 0
+    highest = -1
     artx = ""
     song = ""
+    length = 0
     Track.each do |item|
       if item[:vote_count] > highest
         highest = item[:vote_count]
         artx = item[:artist]
         song = item[:filename]
+        length = item[:length]
       end
     end
-    play(artx, song)
+    this_host = @host_address
+    session = Net::SSH.start( this_host, 'djam', :password => "C#ristmas25" )
+      session.exec "mpc clear ; mpc search artist \"#{artx}\" | grep \'#{song}\' | mpc add ; mpc play"
+    session.close
+
+    # TODO delete playlist entry
+    return length
 
   end
 
