dff85aec1bc54710fd9ca9f8d66df3ea2b8417e6
Denis Tyurkov <dtyurkov@gmail.com>
2015-08-26 12:31:30 +1200

    ADD YouTube Song Scrape

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'httparty'
 gem 'viddl-rb'
 gem 'youtube-downloader'
+gem 'youtube-dl.rb'
 
 gem 'net-ssh', '~> 2.9.2'
 
--- app/models/youtube_scraper.rb
+++ app/models/youtube_scraper.rb
@@ -1,26 +1,23 @@
-require 'open-uri'
-# require 'viddl-rb'
-
 class YoutubeScraper
   include HTTParty
 
-  # # base_uri https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{query}&type=video&videoCategoryId=music&key=AIzaSyBAKGAA_wIMXH_lANamrJ1CScHyRLjmX3Y
-  # AIzaSyBAKGAA_wIMXH_lANamrJ1CScHyRLjmX3Y
+  base_uri "https://www.googleapis.com/youtube/v3"
 
-  # base_uri "https://www.googleapis.com/youtube/v3"s
 
-  def self.scrape_audio(video_id)
-    link = "https://www.youtube.com/watch?v=#{video_id}"
-   p  stuff = ViddlRb.get_urls_names(link).first
-  end
 
   def self.search(query)
     modified_query = query.gsub(/\W/, "+")
     token = "AIzaSyBAKGAA_wIMXH_lANamrJ1CScHyRLjmX3Y"
-    response = HTTParty.get("https://www.googleapis.com/youtube/v3/search?part=snippet&q=#{modified_query}&type=video&videoCategoryId=music&videoDefinition=high&key=#{token}")
-   p video_id = response["items"][0]["id"]["videoId"]
+    response = self.get("/search?part=snippet&maxResults=5&q=#{modified_query}&type=video&videoCategoryId=music&videoDefinition=high&key=#{token}")
+    video_id = response["items"]
+    puts video_id
     # YoutubeScraper.scrape_audio(video_id)
   end
 
+
+  def self.scrape_audio(video_id)
+    link = "https://www.youtube.com/watch?v=#{video_id}"
+    system("youtube-dl --extract-audio --audio-format mp3  --audio-quality  0 #{link}")
+  end
 
 end
