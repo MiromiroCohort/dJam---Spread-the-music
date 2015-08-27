79e6d3e8e2f36d126a4ecb565d351a6cd051575f
Apprentice <apprentice@StMac2.local>
2015-08-27 17:16:33 +1200

    add duration scraper

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/search/new' => 'search#new'
   get '/search/scrape' => 'search#scrape'
   post '/session' => 'session#new'
-  root 'site#index'
+  # root 'site#index'
   resources :users
   resources :hosts
   post '/search/add' => 'search#add'
