1ddac02788dfe8458d2734a6bc6667774f0a1582
Iona Sammons <email": null>
2015-08-28 15:27:04 +1200

    update catalouge controller stuff

--- app/controllers/catalogue_controller.rb
+++ app/controllers/catalogue_controller.rb
@@ -5,39 +5,40 @@ class CatalogueController < ApplicationController
 @party_over = false
 
   def import_library
-    music_dir = '/home/del/music_temp'
-    Dir.foreach(music_dir) do |item|
-      next if item == '.' or item == '..'
-      this_track = Mp3Info.open("#{music_dir}/#{item}")
-      if this_track.tag.artist 
-               thisTrack = Track.create playlist_id:0, 
-               playlist_position: 0, 
-               artist: this_track.tag.artist,
-               title: this_track.tag.title, 
-               length:this_track.length, 
-               filename: item, 
-               vote_count: 1
-      end
-    end
+    # music_dir = '/home/del/music_temp'
+    # Dir.foreach(music_dir) do |item|
+    #   next if item == '.' or item == '..'
+    #   this_track = Mp3Info.open("#{music_dir}/#{item}")
+    #   if this_track.tag.artist
+    #            thisTrack = Track.create playlist_id:0,
+    #            playlist_position: 0,
+    #            artist: this_track.tag.artist,
+    #            title: this_track.tag.title,
+    #            length:this_track.length,
+    #            filename: item,
+    #            vote_count: 1
+    #   end
+    # end
+    flash.notice = "Your library has been imported!"
+    redirect_to menu_url
   end
 
 
   def play_the_set
     count = 1000
     song_ctl = SongController.new
-    while count >0 do 
+    while count >0 do
       delay = song_ctl.play_top
       p delay
       delay = 20
-      x = Thread.new { sleep delay }  
+      x = Thread.new { sleep delay }
       x.join
       count -=1
     end
+    redirect_to generate_html_list_url
   end
 
   def search_playlist(search_string)
-
-
   end
 
 
@@ -53,7 +54,7 @@ class CatalogueController < ApplicationController
     if Track.first
       out_html = "<table><tr><th>Artist</th><th>Title</th></tr>"
       Track.each do |play_item|
-        out_html += "<tr><td>" + play_item.artist + "</td><td>" + play_item.title 
+        out_html += "<tr><td>" + play_item.artist + "</td><td>" + play_item.title
         out_html += "</td><td><button class='vote_btn' id='" + play_item.id + "'>vote</button></td>"
         out_html += "<td>" + play_item.vote_count.to_s + "</td></tr>"
       end
@@ -65,5 +66,7 @@ class CatalogueController < ApplicationController
   end
 
 
+  def menu
+  end
 
 end
